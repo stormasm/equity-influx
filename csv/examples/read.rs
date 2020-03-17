7c8517529d73b99fcc1d962f62c7f45871825ddf
Michael Angerman <stormasm@gmail.com>
2020-03-17 12:08:39 -0700

    data is now in a hashmap

--- csv/examples/read.rs
+++ csv/examples/read.rs
@@ -1,18 +1,24 @@
 use chrono::prelude::*;
 use csv::Reader;
+use std::collections::HashMap;
 use std::error::Error;
 use std::process;
 
 fn example() -> Result<(), Box<dyn Error>> {
     let mut rdr = Reader::from_path("./examples/data/test.csv")?;
     for result in rdr.records() {
+        let mut entry: HashMap<String, String> = HashMap::new();
         let record = result?;
         let timestamp = &record[0];
         let close = &record[4];
         let volume = &record[5];
         let t1 = Utc.datetime_from_str(timestamp, "%Y-%m-%d %H:%M").unwrap();
-        let t2 = t1.timestamp();
-        println!("{:?} {:?} {:?}", t2,close,volume);
+        let t2 = t1.timestamp().to_string();
+        // println!("{:?} {:?} {:?}", t2,close,volume);
+        entry.insert("timestamp".to_string(), t2);
+        entry.insert("close".to_string(), close.to_string());
+        entry.insert("volume".to_string(), volume.to_string());
+        println!("{:?}", entry);
     }
     Ok(())
 }
