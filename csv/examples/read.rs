428d4728dadda683ccd2cc841a10b09c053d5b5a
Michael Angerman <stormasm@gmail.com>
2020-03-17 11:39:13 -0700

    mw

--- csv/examples/read.rs
+++ csv/examples/read.rs
@@ -7,15 +7,11 @@ fn example() -> Result<(), Box<dyn Error>> {
     let mut rdr = Reader::from_path("./examples/data/test.csv")?;
     for result in rdr.records() {
         let record = result?;
-        // println!("{:?}", record);
-        // println!("{:?}", &record[0]);
         let timestamp = &record[0];
-        // println!("{:?}",timestamp);
-        //let t1 = DateTime::parse_from_str(timestamp,"%Y-%m-%d %H:%M");
-        let t2 = Utc.datetime_from_str(timestamp, "%Y-%m-%d %H:%M").unwrap();
+        let t1 = Utc.datetime_from_str(timestamp, "%Y-%m-%d %H:%M").unwrap();
+        println!("{:?}", t1);
+        let t2 = t1.timestamp();
         println!("{:?}", t2);
-        let t3 = t2.timestamp();
-        println!("{:?}", t3);
     }
     Ok(())
 }
