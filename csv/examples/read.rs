d7df85105a7704bd5df3f10bc6babaef3e7bad22
Michael Angerman <stormasm@gmail.com>
2020-03-18 09:58:59 -0700

    up and running

--- csv/examples/read.rs
+++ csv/examples/read.rs
@@ -1,23 +1,14 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 use std::path::{Path, PathBuf};
 
 use chrono::prelude::*;
 use csv::Reader;
-use std::collections::HashMap;
-//use std::error::Error;
-//use std::process;
 
-//#[allow(dead_code)]
 fn read_csv(filename: &str) -> Result<(), Box<dyn Error>> {
-
-//  let mut rdr = Reader::from_path("./examples/data/ui.csv")?;
     let mut rdr = Reader::from_path(filename)?;
-
-    // let measurement = file_stem(filename).unwrap().to_string();
-    // println!("{:?}", measurement);
-
-    let mut vec: Vec<HashMap<String,String>> = Vec::new();
+    let mut vec: Vec<HashMap<String, String>> = Vec::new();
 
     for result in rdr.records() {
         let mut entry: HashMap<String, String> = HashMap::new();
@@ -27,55 +18,43 @@ fn read_csv(filename: &str) -> Result<(), Box<dyn Error>> {
         let volume = &record[5];
         let t1 = Utc.datetime_from_str(timestamp, "%Y-%m-%d %H:%M").unwrap();
         let t2 = t1.timestamp().to_string();
-        // println!("{:?} {:?} {:?}", t2,close,volume);
-        entry.insert("measurement".to_string(), file_stem(filename).unwrap().to_string());
+        entry.insert(
+            "measurement".to_string(),
+            file_stem(filename).unwrap().to_string(),
+        );
         entry.insert("timestamp".to_string(), t2);
         entry.insert("close".to_string(), close.to_string());
         entry.insert("volume".to_string(), volume.to_string());
-        println!("{:?}", entry);
         vec.push(entry);
     }
-    println!("{:?}",vec);
+    println!("{:?}\n", vec);
     Ok(())
 }
 
 fn dir_reader(mydir: String) -> Result<Vec<PathBuf>, Box<dyn Error>> {
     let mypath = Path::new(&mydir);
-    println!("Entries in {:?}:", mypath);
-
     let mut vec: Vec<PathBuf> = Vec::new();
 
     for entry in fs::read_dir(mypath)? {
         let entry = entry?;
         let path = entry.path();
-        //println!("{:?}", path.file_name().ok_or("No filename")?);
         vec.push(path);
     }
 
     Ok(vec)
 }
 
-// #[allow(dead_code)]
 fn file_stem(filename: &str) -> Option<&str> {
     let path = Path::new(filename);
     let name = path.file_stem().unwrap().to_str();
-    println!("{:?}", name);
     name
 }
 
 fn processor(mydir: String) -> Result<(), Box<dyn Error>> {
     let vec = dir_reader(mydir).unwrap();
-    // println!("vec len = {:?}", vec.len());
-
-    //let _x = read_csv("./examples/data/ui.csv");
-
     for name in vec {
-        // let filename = name.file_name().ok_or("No filename")?;
         let filename = name.to_str().unwrap();
         let _x = read_csv(filename);
-        let filestem = name.file_stem();
-        println!("{:?} {:?}", filename, filestem);
-        // println!("{:?}", file_stem(filename));
     }
 
     Ok(())
