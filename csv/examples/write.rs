7cffa2ff6da4e400104d73494726ce7b70d3cea2
Michael Angerman <stormasm@gmail.com>
2020-03-19 10:12:30 -0700

    cc

--- csv/examples/write.rs
+++ csv/examples/write.rs
@@ -1,11 +1,7 @@
 use std::collections::HashMap;
 use std::error::Error;
-//use std::fs;
-//use std::path::{Path, PathBuf};
 
-fn write_lp(dirname: &str, vec: Vec<HashMap<String, String>>) -> Result<(), Box<dyn Error>> {
-    println!("{:?}\n", dirname);
-    //println!("{:?}\n", vec);
+fn write_lp(_dirname: &str, vec: Vec<HashMap<String, String>>) -> Result<(), Box<dyn Error>> {
     for line in vec {
         println!("{:?}\n", line);
     }
@@ -41,9 +37,6 @@ fn get_vector() -> Vec<HashMap<String, String>> {
     vec![foo, bar, baz]
 }
 
-//Ref
-//https://stackoverflow.com/questions/49452040/how-can-a-vector-of-hashmaps-be-created-and-returned-from-main
-
 fn main() {
     let dirout = String::from("./tmp/out");
     let vec = get_vector();
@@ -51,3 +44,4 @@ fn main() {
 }
 
 // let mut vec = vec![{"volume": "344000.00", "close": "127.85", "timestamp": "1583712000", "measurement": "ui"}, {"close": "135.63", "measurement": "ui", "volume": "240900.00", "timestamp": "1583798400"}, {"measurement": "ui", "close": "129.50", "timestamp": "1583884800", "volume": "336700.00"}, {"measurement": "ui", "volume": "411600.00", "timestamp": "1583971200", "close": "113.45"}, {"close": "122.92", "volume": "440800.00", "measurement": "ui", "timestamp": "1584057600"}];
+// https://stackoverflow.com/questions/49452040/how-can-a-vector-of-hashmaps-be-created-and-returned-from-main
